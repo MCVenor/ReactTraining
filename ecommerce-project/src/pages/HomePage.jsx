0bb374d9d7eb122197c552366d12656399033783
MCVenor <michal.j.cu@gmail.com>
2026-03-30 19:44:13 +0200

    fixec Proxy Erroradd server proxy config for /api and /images

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -9,17 +9,16 @@ export function HomePage() {
     const [cart, setCart] = useState([]);
 
     useEffect(() => {
-        axios.get('http://localhost:3000/api/products')
+        axios.get('/api/products')
             .then((response) => {
                 setProducts(response.data);
             })
 
-        axios.get('http://localhost:3000/api/cart-items')
+        axios.get('/api/cart-items')
             .then((response) => {
                 setCart(response.data);
             })
     }, []);
-
 
 
     return (
@@ -28,10 +27,8 @@ export function HomePage() {
 
             <Header cart={cart}/>
 
-
             <div className="home-page">
                 <div className="products-grid">
-
                     {products.map((product) => {
                         return (
                             <>
