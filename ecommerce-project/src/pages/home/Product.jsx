86452e0c8e745dd2e62f6d7579a8e99db9b5ab83
MCVenor <michal.j.cu@gmail.com>
2026-04-05 14:12:15 +0200

    Create IntegrationTest for product component

--- ecommerce-project/src/pages/home/Product.jsx
+++ ecommerce-project/src/pages/home/Product.jsx
@@ -23,6 +23,7 @@ export function Product({ product, loadCart }) {
         <div className="product-container">
             <div className="product-image-container">
                 <img className="product-image"
+                    data-testid="product-image"
                     src={product.image} />
             </div>
 
@@ -38,6 +39,7 @@ export function Product({ product, loadCart }) {
                                         })} */}
 
                 <img className="product-rating-stars"
+                    data-testid="product-rating-starts-image"
                     src={`images/ratings/rating-${product.rating.stars * 10}.png`} />
                 <div className="product-rating-count link-primary">
                     {product.rating.count}
